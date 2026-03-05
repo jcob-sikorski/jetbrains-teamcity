a18d91eb9d06d2649032d7b13ceeecd035d94144
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2026-03-06 00:20:53 +0100

    init

---
+++ teamcity-cloud-vm-autoscaler/src/main/java/io/github/jakubsiekiera/teamcity_cloud_vm_autoscaler/TeamcityCloudVmAutoscalerApplication.java
@@ -0,0 +1,13 @@
+package io.github.jakubsiekiera.teamcity_cloud_vm_autoscaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityCloudVmAutoscalerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamcityCloudVmAutoscalerApplication.class, args);
+	}
+
+}
---
+++ teamcity-cloud-vm-autoscaler/src/test/java/io/github/jakubsiekiera/teamcity_cloud_vm_autoscaler/TeamcityCloudVmAutoscalerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jakubsiekiera.teamcity_cloud_vm_autoscaler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcityCloudVmAutoscalerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
